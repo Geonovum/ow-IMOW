ae27353b7efa2290b98b20690b16cc45caf75e44
Geonovum <>
2023-09-01 13:47:07 +0000

    Update from https://github.com/Geonovum/word2werkversie/commit/28513877d5ad3af5e42765a387756094e8c793d6

--- js/document-config.js
+++ js/document-config.js
@@ -3,18 +3,31 @@ var documentConfig =
   title: "Informatiemodel Omgevingswet (IMOW)",
   shortName: "IMOW",
   pubDomain: "ow",
-  specStatus: "GN-WV",
-  specType: "ST",
+  specStatus: "geen",
+  specType: "st",
   license: "cc-by-nd",
   latestVersion: [
     "docs.geostandaarden.nl/ow/IMOW/"
   ],
-  publishDate: "2022-06-03",
+  publishDate: "2023-09-01",
+  publishVersion: "2.0.3",
   thisVersion: [
-    "docs.geostandaarden.nl/ow/gn-wv-st-IMOW-20220603/"
+    "docs.geostandaarden.nl/ow/geen-st-IMOW-20230901/"
   ],
   github: "https://github.com/geonovum/ow-IMOW/",
   issueBase: "https://github.com/geonovum/ow-IMOW/issues/",
-  authors: "",
-  editors: ""
+  authors: [
+    {
+      name: "TPOD-team",
+      company: "Geonovum",
+      companyURL: "www.geonovum.nl"
+    }
+  ],
+  editors: [
+    {
+      name: "TPOD-team",
+      company: "Geonovum",
+      companyURL: "www.geonovum.nl"
+    }
+  ]
 }
