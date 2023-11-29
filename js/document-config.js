fcf7f52f9cf32ac883535c85ca6f41e4a82a96ae
Geonovum <>
2023-11-29 09:59:07 +0000

    Update from https://github.com/Geonovum/word2werkversie/commit/cee7955e7f6cc1b1442577940c4f9d41d9413f1c

--- js/document-config.js
+++ js/document-config.js
@@ -9,7 +9,7 @@ var documentConfig =
   latestVersion: [
     "https://docs.geostandaarden.nl/ow/IMOW/"
   ],
-  publishDate: "2023-11-28",
+  publishDate: "2023-11-29",
   publishVersion: "2.0.3",
   thisVersion: [
     "https://geonovum.github.io/ow-IMOW/"
