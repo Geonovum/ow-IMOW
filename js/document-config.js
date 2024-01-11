8bfc24a74209d0b0d92c504573c5e0fcbefed75b
Geonovum <>
2024-01-11 10:35:20 +0000

    Update from https://github.com/Geonovum/word2werkversie/commit/791129bcb2bfcc88d7fc0e98cf215b3d494e707f

--- js/document-config.js
+++ js/document-config.js
@@ -3,21 +3,21 @@ var documentConfig =
   title: "Informatiemodel Omgevingswet (IMOW)",
   shortName: "IMOW",
   pubDomain: "ow",
-  specStatus: "def",
+  specStatus: "wv",
   specType: "im",
   license: "cc-by-nd",
   latestVersion: [
     "https://docs.geostandaarden.nl/ow/IMOW/"
   ],
-  publishDate: "2023-12-27",
-  publishVersion: "3.0.1",
+  publishDate: "2024-01-11",
+  publishVersion: "werkversie",
   thisVersion: [
-    "https://docs.geostandaarden.nl/ow/def-im-IMOW-20231227/"
+    "https://geonovum.github.io/ow-IMOW/"
   ],
-  previousPublishDate: "2023-12-15",
-  previousPublishVersion: "3.0.0",
+  previousPublishDate: "2023-12-27",
+  previousPublishVersion: "3.0.1",
   prevVersion: [
-    "https://docs.geostandaarden.nl/ow/def-im-IMOW-20231215/"
+    "https://docs.geostandaarden.nl/ow/def-im-IMOW-20231227/"
   ],
   previousMaturity: "def",
   github: "https://github.com/geonovum/ow-IMOW/",
